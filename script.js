2fdb7e04072fbb87f6bc65dccf4bdc63edaa1c2b
eyeignite <eyeignite402@gmail.com>
2023-09-22 11:02:12 +0100

    add game function which loops until 5 rounds are played and displays winner

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let draws = 0;
 
 let playerWinRound = "Player wins!";
 let computerWinRound = "Computer Wins!";
+let playerWinsGame = "Play has won the game, congrats!";
+let computerWinsGame = "Computer has won the game, congrats!";
 
 function getComputerChoice() {
   const randomChoice = Math.floor(Math.random() * 3);
@@ -21,25 +23,43 @@ function getComputerChoice() {
 
 function playRound(playerSelection, computerSelection) {
 
-  if (playerSelection === computerSelection) {
-    return draw;
+  if (playerSelection === "" || playerSelection === null) {
+    console.log("Invalid input. Please enter rock, paper, or scissors")
+  }
+  else if (playerSelection === computerSelection) {
+    return `Draw! Both players picked ${playerSelection}`;
+    draws++
   }
   else if (playerSelection === "rock" && computerSelection === "scissors") {
     return playerWinRound + " Rock beats scissors!";
+    playerScore++
   }
   else if (playerSelection === "paper" && computerSelection === "rock") {
     return playerWinRound + " Paper beats rock!";
+    playerScore++
   }
   else if (playerSelection === "scissors" && computerSelection === "paper") {
-    return playerWinRound + " Scissors beats paper!"; 
+    return playerWinRound + " Scissors beats paper!";
+    playerScore++
   }
   else {
     return computerWinRound + ` ${computerSelection.charAt(0).toUpperCase() + computerSelection.slice(1)} beats ${playerSelection}!`;
+    computerScore++
   }
 }
 
-const playerSelection = prompt("Enter Rock, Paper, or Scissors to start the game").toLowerCase();
-let draw = `Draw! Both players picked ${playerSelection}`;
-const computerSelection = getComputerChoice();
 
-console.log( playRound(playerSelection, computerSelection) );
+function game() {
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = prompt("Enter Rock, Paper, or Scissors to start the game").toLowerCase();
+    const computerSelection = getComputerChoice();
+    const result = playRound(playerSelection, computerSelection);
+    console.log(result);
+
+  }
+}
+  
+
+
+
+console.log( game() );
