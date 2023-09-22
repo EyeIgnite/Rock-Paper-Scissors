b2ec3260a9360f6bacf0ddae2c9088667db19aa1
eyeignite <eyeignite402@gmail.com>
2023-09-22 09:25:42 +0100

    create variables and a function for player and computer selections

---
+++ script.js
@@ -0,0 +1,22 @@
+function getComputerChoice() {
+  const randomChoice = Math.random() * 3;
+
+  if (randomChoice <= 1) {
+    return 1; // rock
+  }
+  else if (randomChoice <= 2) {
+    return 2; // paper
+  }
+  else {
+    return 3; // scissors
+  }
+}
+
+function playRound(playerSelection, computerSelection) {
+
+
+}
+
+const playerSelection = prompt("Rock, Paper or Scissors to start the game").toLowerCase();
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
